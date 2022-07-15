8f0bef9e8bed82324bfecbc51e2a8f515d877bf2
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2022-07-15 02:30:59 -0300

    feat: group routes

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/contato', 'ContatoController@contato');
 
 Route::get('/login', function () { return 'Login'; });
 
-Route::get('/clientes', function () { return 'Clientes'; });
-
-Route::get('/fornecedores', function () { return 'Fornecedores'; });
-
-Route::get('/produtos', function () { return 'Produtos'; });
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () { return 'Clientes'; });
+    Route::get('/fornecedores', function () { return 'Fornecedores'; });
+    Route::get('/produtos', function () { return 'Produtos'; });
+});
