83768ec46c435ce1e060adaef6fb990d4b7dec63
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2022-07-15 01:46:42 -0300

    feat: add new controllers

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Olá mundo';
-});
+Route::get('/', 'PrincipalController@principal');
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre nos';
-});
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
-Route::get('/contato', function () {
-    return 'Contato';
-});
+Route::get('/contato', 'ContatoController@contato');
