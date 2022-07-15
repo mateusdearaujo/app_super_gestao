5e12bf5ae5cc4e94158e34d9ba3deadb0a1c6798
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2022-07-15 02:11:00 -0300

    chore: add views

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class SobreNosController extends Controller
 {
     public function sobreNos()
     {
-        echo 'Sobre nós';
+        return view('site.sobre-nos');
     }
 }
